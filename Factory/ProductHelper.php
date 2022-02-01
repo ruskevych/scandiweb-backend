f3066824a926794689b3bfef7e0af986072d2d91
ruskevych <33175520+ruskevych@users.noreply.github.com>
2022-02-01 02:05:30 +0200

    Update ProductHelper.php

--- Factory/ProductHelper.php
+++ Factory/ProductHelper.php
@@ -4,24 +4,12 @@ namespace scandiweb\Factory;
 
 class ProductHelper
 {
-    public static function getProductFactory(string $type): ProductFactoryInterface
-    {
-        switch($type){
-            case 'DVD' : {
-                return new DiskProductFactory();
-            }
-            case 'Book' : {
-                return new BookProductFactory();
-            }
-            case 'Furniture' : {
-                return new FurnitureProductFactory();
-            }
-            default: {
-                die (json_encode(array(
-                    'field' => 'type',
-                    'error' => 'is required in format Book | DVD | Furniture'
-                )));
-            }
-        }
-    }
+        $typeValues = ['DVD' => 'scandiweb\Factory\DiskProductFactory', 
+                       'Book' => 'scandiweb\Factory\BookProductFactory', 
+                       'Furniture' => 'scandiweb\Factory\FurnitureProductFactory'];
+
+        $productFactory = $typeValues[$type];
+        return new $productFactory();
+
+
 }
